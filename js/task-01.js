d8eee5ce9708adce628d08876f2f6645fdecb082
NadiaOl <115547708+NadiaOl@users.noreply.github.com>
2023-09-17 19:59:48 +0300

    lead-up

---
+++ js/task-01.js
@@ -0,0 +1,29 @@
+// Посчитает и выведет в консоль количество категорий в ul#categories,
+// то есть элементов li.item.
+
+const ulEl = document.querySelectorAll('#categories > li');
+console.log("Number of categories: ", ulEl.length);
+
+
+// Для каждого элемента li.item в списке ul#categories,
+// найдет и выведет в консоль текст заголовка элемента(тега < h2 >)
+//  и количество элементов в категории(всех вложенных в него < li >).
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+ulEl.forEach((el, i) => {
+    const categoryText = 
+    console.log("Category: ", el.firstElementChild.textContent);
+    const categoryLength =
+    console.log("Elements: ", el.lastElementChild.children.length);
+})
